e4d49a4d722b9d4e56c49fb9415a3dd31eda201d
Easz10 <estesolis10@gmail.com>
2023-02-02 18:18:02 -0600

    Inicialización del proyecto

---
+++ Semana2/src/main.py
@@ -0,0 +1,15 @@
+print("Hola mundo")
+
+n = 10
+
+print(n)
+
+#Para comentar una sola línea de código se una #
+'''
+Para comentar un bloque de texto o varias líneas de texto
+se usan tres comillas al inicio y al final
+o también se puede usar el comando ctrl + k + c
+y para descomentar se usa ctrl + k + v
+'''
+
+print("Cuba")
